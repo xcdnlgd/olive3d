7596ce454e629b0002f1d8bbc048602db7eedc09
xcdnlgd <xcdnlgd@hotmail.com>
2024-12-07 15:48:29 +0800

    improve fill_triangle performance

--- src/renderer.rs
+++ src/renderer.rs
@@ -61,37 +61,32 @@ impl<'b> Renderer<'b> {
     }
     pub fn fill_triangle(&mut self, verts: &[Vector3], shader: impl Fn(Vector3) -> u32) {
         let ((x_min, y_min), (x_max, y_max)) = triangle_bunding_box(verts);
-        if let Some(((x_min, y_min), (x_max, y_max))) = normalize_rect(
-            x_min as i32,
-            y_min as i32,
-            (x_max - x_min) as i32 + 1,
-            (y_max - y_min) as i32 + 1,
-            self.width,
-            self.height,
-        ) {
-            for y in y_min..=(y_max + 1) {
-                for x in x_min..=(x_max + 1) {
-                    let bc = barycentric(
-                        x as f32 + 0.5,
-                        y as f32 + 0.5,
-                        verts[0].x(),
-                        verts[0].y(),
-                        verts[1].x(),
-                        verts[1].y(),
-                        verts[2].x(),
-                        verts[2].y(),
-                    );
-                    if bc.x() < 0.0 || bc.y() < 0.0 || bc.z() < 0.0 {
-                        continue;
-                    }
-                    let mut z = 0.0;
-                    for i in 0..3 {
-                        z += verts[i].z() * bc[i]
-                    }
-                    if self.z_buffer[(x + y * self.stride) as usize] < z {
-                        self.z_buffer[(x + y * self.stride) as usize] = z;
-                        self.draw_pixel_unchecked(x, y, shader(bc));
-                    }
+        let x_min = (x_min.round() as i32).max(0) as u32;
+        let y_min = (y_min.round() as i32).max(0) as u32;
+        let x_max = (x_max.round() as i32).min(self.width as i32) as u32;
+        let y_max = (y_max.round() as i32).min(self.height as i32) as u32;
+        for y in y_min..y_max {
+            for x in x_min..x_max {
+                let bc = barycentric(
+                    x as f32 + 0.5,
+                    y as f32 + 0.5,
+                    verts[0].x(),
+                    verts[0].y(),
+                    verts[1].x(),
+                    verts[1].y(),
+                    verts[2].x(),
+                    verts[2].y(),
+                );
+                if bc.x() < 0.0 || bc.y() < 0.0 || bc.z() < 0.0 {
+                    continue;
+                }
+                let mut z = 0.0;
+                for i in 0..3 {
+                    z += verts[i].z() * bc[i]
+                }
+                if self.z_buffer[(x + y * self.stride) as usize] < z {
+                    self.z_buffer[(x + y * self.stride) as usize] = z;
+                    self.draw_pixel_unchecked(x, y, shader(bc));
                 }
             }
         }
@@ -107,41 +102,6 @@ fn triangle_bunding_box(verts: &[Vector3]) -> ((f32, f32), (f32, f32)) {
     xs.sort_by(|a, b| a.partial_cmp(b).unwrap());
     ys.sort_by(|a, b| a.partial_cmp(b).unwrap());
     ((xs[0], ys[0]), (xs[2], ys[2]))
-}
-
-/// ```
-/// if let Some((x0, y0), (x1, y1)) = normalize_rect(x, y, w, h, bound_width, bound_height) {
-///     for y in y0..=y1 {
-///         for x in x0..=x1 {
-///             // do things on (x, y)
-///         }
-///     }
-/// }
-/// ```
-fn normalize_rect(
-    x: i32,
-    y: i32,
-    w: i32,
-    h: i32,
-    bound_width: u32,
-    bound_height: u32,
-) -> Option<((u32, u32), (u32, u32))> {
-    if w == 0 || h == 0 {
-        return None;
-    }
-    let x1 = if w > 0 { x + w - 1 } else { x + w + 1 };
-    let y1 = if h > 0 { y + h - 1 } else { y + h + 1 };
-    let mut x0 = x.clamp(0, bound_width as i32 - 1);
-    let mut y0 = y.clamp(0, bound_height as i32 - 1);
-    let mut x1 = x1.clamp(0, bound_width as i32 - 1);
-    let mut y1 = y1.clamp(0, bound_height as i32 - 1);
-    if x1 < x0 {
-        std::mem::swap(&mut x0, &mut x1);
-    }
-    if y1 < y0 {
-        std::mem::swap(&mut y0, &mut y1);
-    }
-    Some(((x0 as u32, y0 as u32), (x1 as u32, y1 as u32)))
 }
 
 // return (u, v, w)
